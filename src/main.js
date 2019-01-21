040cb16ed292b820559a8d71499165a20ba65570
Little Monster <monster860313@gmail.com>
2019-01-22 02:11:53 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import VueRouter from 'vue-router'
+import App from './App'
+import store from './store/index'
+import AuthHandler from './components/AuthHandler'
+import ImageList from './components/ImageList'
+import UploadForm from './components/UploadForm'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: ImageList },
+    { path: '/upload', component: UploadForm },
+    { path: '/oauth2/callback', component: AuthHandler }
+  ]
+})
 
 new Vue({
   router,
